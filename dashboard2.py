5951e157be4e11760d2b1db9927a02d4d81c305c
xxxtopher <125408374+xxxtopher@users.noreply.github.com>
2023-02-14 15:41:55 -0800

    Update dashboard2.py

--- dashboard2.py
+++ dashboard2.py
@@ -2,6 +2,8 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
+st.write("Total Deal Value by Type by Industry")
+
 df = pd.read_excel("Data Manipulation Worksheet.xlsx", sheet_name='Financing Table Clean', parse_dates=['DATE'], index_col=['DATE'])
 @st.cache
 # Manipulate the data
