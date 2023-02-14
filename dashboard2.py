e375f869b9bd46bbd11ac95f26db7879a6b38b1e
xxxtopher <125408374+xxxtopher@users.noreply.github.com>
2023-02-14 15:31:09 -0800

    Update dashboard2.py

--- dashboard2.py
+++ dashboard2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import portly.express as px
+import plotly.express as px
 
 df = pd.read_excel("Data Manipulation Worksheet.xlsx", sheet_name='Financing Table Clean', parse_dates=['DATE'], index_col=['DATE'])
 
