16b3d645636c26df45fe29f149eb0454b4cd21d8
xxxtopher <125408374+xxxtopher@users.noreply.github.com>
2023-02-14 14:01:36 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ df = pd.read_csv(ticker + ".csv", parse_dates=['Date'], index_col=['Date'])
 begDate = df.index.min()
 endDate = df.index.max()
 
+minDateDropdown = pd.to_datetime('2010-01-01')
+
 pickStart = st.date_input("Pick start date", begDate)
 pickEnd = st.date_input("Pick end date:", endDate)
 
