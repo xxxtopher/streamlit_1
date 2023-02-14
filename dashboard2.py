9e84ecd569cdc3a9111f733e4094183a41fb2354
xxxtopher <125408374+xxxtopher@users.noreply.github.com>
2023-02-14 15:36:33 -0800

    Update dashboard2.py

--- dashboard2.py
+++ dashboard2.py
@@ -3,10 +3,10 @@ import streamlit as st
 import plotly.express as px
 
 df = pd.read_excel("Data Manipulation Worksheet.xlsx", sheet_name='Financing Table Clean', parse_dates=['DATE'], index_col=['DATE'])
-
+@st.cache
 # Manipulate the data
 finDeals = df.groupby(['TYPE', 'INDUSTRY'])['SIZE'].sum().reset_index()
 
 fig = px.bar(finDeals, x='TYPE', y='SIZE', color='INDUSTRY',
              title='Total Deal Value by Type by Industry')
-fig.show()
+st.plotly_chart(fig)
