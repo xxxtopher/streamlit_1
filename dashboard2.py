e3c488a1a744e4340e034d589d4f0e0f8a659bb5
xxxtopher <125408374+xxxtopher@users.noreply.github.com>
2023-02-14 16:13:50 -0800

    Update dashboard2.py

--- dashboard2.py
+++ dashboard2.py
@@ -4,9 +4,7 @@ import plotly.express as px
 
 st.write("Total Deal Value by Type by Industry")
 
-df = pd.read_excel('Data Manipulation Worksheet.xlsx', sheet_name='Financing Table Clean', parse_dates=['DATE'], index_col=['DATE'])
-
-findeals = df.groupby(['TYPE', 'INDUSTRY'])['SIZE'].sum().reset_index()
+findeals = pd.read_excel('Data Manipulation Worksheet.xlsx', sheet_name='Financing Table Clean', parse_dates=['DATE'], index_col=['DATE'])
 
 fig = px.bar(findeals, x='TYPE', y='SIZE', color='INDUSTRY',
              title='Total Deal Value by Type by Industry')
