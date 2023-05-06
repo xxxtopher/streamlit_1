931e754896a1ed4f65c03fe8fe59988cc6f8da07
xxxtopher <125408374+xxxtopher@users.noreply.github.com>
2023-05-06 12:34:33 -0700

    Update newstest.py

--- newstest.py
+++ newstest.py
@@ -1,48 +1,61 @@
 import streamlit as st
-from newsapi import NewsApiClient
-from bs4 import BeautifulSoup
+import yfinance as yf
+import plotly.graph_objs as go
+import pandas as pd
 import requests
+from bs4 import BeautifulSoup
+from newsapi import NewsApiClient
 
-# Initialize NewsAPI client with API key
+# Set up NewsAPI
 newsapi = NewsApiClient(api_key='7b36370fdca94d0eba309efc7819b48c')
 
-def search_news(stock_ticker):
-    query = stock_ticker + ' 股票 新闻'
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
-    news_links = []
-    for url in search(query, num_results=5, lang='zh-CN', pause=2):
-        if 'aastocks' in url or 'etnet' in url or 'hkej' in url:
-            html = requests.get(url, headers=headers).text
-            soup = BeautifulSoup(html, 'html.parser')
-            for link in soup.find_all('a', href=True):
-                if 'aastocks' in link['href'] or 'etnet' in link['href'] or 'hkej' in link['href']:
-                    if len(link['href'].split('?')) > 1:
-                        url = link['href'].split('?')[0].split('/url?q=')[1]
-                        news_links.append(url)
-    news = []
-    for link in news_links:
-        try:
-            article = Article(link)
-            article.download()
-            article.parse()
-            news.append(article.text)
-        except:
-            continue
-    return news
+# Define a function to get news headlines
+def get_news_headlines(query):
+    news_headlines = []
+    news_articles = newsapi.get_everything(q=query, sort_by='relevancy', language='en', page_size=5)['articles']
+    for article in news_articles:
+        news_headlines.append(article['title'])
+    return news_headlines
 
-# Define Streamlit app
-st.title('Stock News Search')
-st.write('This app searches for news articles related to a stock ticker.')
-stock_ticker = st.text_input('Enter a stock ticker (e.g. AAPL)')
+# Define a function to get stock news from Yahoo Finance
+def get_stock_news(ticker):
+    stock_news = []
+    url = f"https://finance.yahoo.com/quote/{ticker}/news?p={ticker}"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content)
+    for item in soup.find_all('div', {'class': 'Mb(5px)'}):
+        stock_news.append(item.a.text)
+    return stock_news
 
-if st.button('Search'):
-    # Search for news articles
-    news = search_news(stock_ticker)
-    
-    # Display news articles in a table
-    st.write(f'Showing news articles for {stock_ticker}:')
-    for article in news:
-        st.write('---')
-        st.write(article['title'])
-        st.write(article['description'])
-        st.write(article['url'])
+# Define the Streamlit app
+st.title("Stock Price Dashboard")
+st.write("Enter a stock symbol to see its current price and recent news.")
+
+# Get user input for stock symbol
+ticker = st.text_input("Enter stock symbol (e.g. AAPL):")
+
+# Get stock data
+stock_data = yf.Ticker(ticker).history(period='1y')
+latest_price = stock_data['Close'][-1]
+
+# Display stock chart
+fig = go.Figure(data=[go.Candlestick(x=stock_data.index,
+                open=stock_data['Open'],
+                high=stock_data['High'],
+                low=stock_data['Low'],
+                close=stock_data['Close'])])
+st.plotly_chart(fig)
+
+# Get news headlines for the stock
+news_headlines = get_news_headlines(ticker)
+stock_news = get_stock_news(ticker)
+
+# Display news headlines
+st.write(f"Latest price for {ticker}: {latest_price}")
+st.write("News Headlines:")
+for headline in news_headlines:
+    st.write(headline)
+
+st.write("Stock News:")
+for news in stock_news:
+    st.write(news)
