d822e2bca68cf402b7e0c4ee140d1b67ff553fbe
xxxtopher <125408374+xxxtopher@users.noreply.github.com>
2023-07-22 22:20:27 -0700

    Update pxgraph.py

--- pxgraph.py
+++ pxgraph.py
@@ -60,9 +60,6 @@ def fetch_news_data(stock_ticker):
 st.set_page_config(page_title="Stock Analysis Dashboard", page_icon=":chart_with_upwards_trend:", layout="wide")
 st.title("Stock Analysis Dashboard")
 
-# Create a sidebar for input widgets
-st.sidebar.title("Stock Analysis Options")
-
 # Get user input for stock ticker and date range
 selected_ticker = st.sidebar.text_input("Enter stock ticker (e.g. AAPL):")
 
@@ -76,20 +73,24 @@ end_date = st.sidebar.date_input("Enter end date:", current_date)
 default_start_date = current_date - timedelta(days=365*5)
 start_date = st.sidebar.date_input("Enter start date:", default_start_date)
 
-# Buttons for selecting time frames
-time_frame = st.sidebar.radio("Select Time Frame:", ["1 Month", "3 Months", "1 Year", "3 Years", "5 Years"])
-
-# Determine the appropriate start and end dates based on the selected time frame
-if time_frame == "1 Month":
-    start_date = current_date - timedelta(days=30)
-elif time_frame == "3 Months":
-    start_date = current_date - timedelta(days=90)
-elif time_frame == "1 Year":
-    start_date = current_date - timedelta(days=365)
-elif time_frame == "3 Years":
-    start_date = current_date - timedelta(days=365*3)
-elif time_frame == "5 Years":
-    start_date = current_date - timedelta(days=365*5)
+# Add buttons for selecting time frames above the chart
+st.beta_container()
+col1, col2, col3, col4, col5 = st.beta_columns(5)
+with col1:
+    if st.button("1 Month"):
+        start_date = current_date - timedelta(days=30)
+with col2:
+    if st.button("3 Months"):
+        start_date = current_date - timedelta(days=90)
+with col3:
+    if st.button("1 Year"):
+        start_date = current_date - timedelta(days=365)
+with col4:
+    if st.button("3 Years"):
+        start_date = current_date - timedelta(days=365*3)
+with col5:
+    if st.button("5 Years"):
+        start_date = current_date - timedelta(days=365*5)
 
 if selected_ticker and start_date and end_date:
 
