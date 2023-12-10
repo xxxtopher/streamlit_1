8cb99d261f3f93c2f8752d4d3c7a2e72b4cbac3a
xxxtopher <125408374+xxxtopher@users.noreply.github.com>
2023-12-09 20:50:45 -0800

    Update NFP_st.py

--- NFP_st.py
+++ NFP_st.py
@@ -8,7 +8,7 @@ import matplotlib.dates as mdates
 # Assuming the 'hourly_earnings_data.csv' file has a 'Date' column
 df = pd.read_csv('hourly_earnings_data.csv')
 df['Date'] = pd.to_datetime(df['Date'])  # Convert 'Date' to datetime if it's not already
-df = df.sort_values(by='Date')
+df = df.sort_values(by='Date')  # Sort by 'Date'
 
 # Streamlit App
 st.title("Nonfarm Payrolls Monthly Change")
@@ -17,24 +17,12 @@ st.title("Nonfarm Payrolls Monthly Change")
 fig, ax = plt.subplots(figsize=(10, 6))
 ax.plot(df['Date'], df['Change in Nonfarm Payrolls'])
 
-# Annotate the latest data point
-latest_date = df['Date'].iloc[-1]
-latest_value = df["Change in Nonfarm Payrolls"].iloc[-1]
-ax.annotate(f'Latest Change: {latest_value:.0f}K on {latest_date}',
-            xy=(latest_date, latest_value),
-            xytext=(20, 30),
-            textcoords='offset points',
-            arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"),
-            fontsize=12)
-
 # Format x-axis as dates
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
 plt.xticks(rotation=45)
 
 # Display the plot
 st.pyplot(fig)
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
 
